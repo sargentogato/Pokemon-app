11addd98d1e0a61d34e3cbfe57c910fec1cbc8d7
sargentogato <marlonjvelasquez@gmail.com>
2025-01-26 23:48:45 +0100

    add condition to avoid undefined error indent code

--- src/modules/pokemon/composables/usePokemonGame.ts
+++ src/modules/pokemon/composables/usePokemonGame.ts
@@ -4,17 +4,18 @@ import { GameStatus, type IPokemon, type IPokemonListResponse } from "../interfa
 
 export const usePokemonGame = () => {
   const gameStatus = ref<GameStatus>(GameStatus.Playing);
+  const pokemons = ref<IPokemon[]>([]);
+  const pokemonOptions = ref<IPokemon[]>();
 
-  const pokemons = ref<IPokemon[]>([])
-  const pokemonOptions = ref<IPokemon[]>()
+  const isLoading = computed(() => pokemons.value.length === 0);
 
-  const isLoading = computed(() => pokemons.value.length === 0)
+  const randomPokemon = computed(() => {
+    if (!pokemonOptions.value?.length) return
 
-  const randomIndex = computed(() => {
     const selectedPokemon = Math.floor(Math.random() * pokemonOptions.value!.length);
 
-    return pokemonOptions.value![selectedPokemon!].name
-  })
+    return pokemonOptions.value![selectedPokemon!].name;
+  });
 
   // const randomPokemon = computed(() => pokemonOptions.value![Math.floor(Math.random() * pokemonOptions.value!.length)].name)
 
@@ -26,19 +27,19 @@ export const usePokemonGame = () => {
     return shufflePokemonList(pokemonList);
   };
 
-  const extractPokemonNameAndId = (response:IPokemonListResponse) => {
+  const extractPokemonNameAndId = (response: IPokemonListResponse) => {
     const pokemonArray = response.results.map(pokemon => {
       const urlParts = pokemon.url.split('/');
-      const id = urlParts.at(-2) ?? 0
+      const id = urlParts.at(-2) ?? 0;
 
       return {
         name: pokemon.name,
         id:   +id,
-      }
-    })
+      };
+    });
 
-    return pokemonArray
-  }
+    return pokemonArray;
+  };
 
   const shufflePokemonList = (arr: IPokemon[]) => {
     const arrCopy = [...arr];
@@ -50,25 +51,25 @@ export const usePokemonGame = () => {
     }
 
     return arrCopy;
-  }
+  };
 
   const getNextOptions = (howMany: number = 4) => {
     gameStatus.value = GameStatus.Playing;
     pokemonOptions.value = pokemons.value.slice(0, howMany); //Devuelve los primeros 4 elementos(0,1,2,3)
     pokemons.value = pokemons.value.slice(howMany); //Devuelve el array con los elementos a partir del index 4 (no incluye el 4)
-  }
+  };
 
   onMounted(async () => {
-    await new Promise (response => setTimeout(response, 2000))
+    await new Promise(response => setTimeout(response, 1000));
 
     pokemons.value = await getPokemons();
-    getNextOptions()
+    getNextOptions();
   });
 
   return {
     gameStatus,
     isLoading,
-    randomPokemon: randomIndex,
+    randomPokemon,
 
     //Methods
     getNextOptions,
